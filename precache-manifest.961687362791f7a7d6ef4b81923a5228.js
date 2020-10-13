8b9534c7a1f7f8c57575bf7b38f4aec2caff4e28
Joëlle <new@Jolle.Jolle>
2020-10-13 11:17:47 +0300

    Updates

---
+++ precache-manifest.961687362791f7a7d6ef4b81923a5228.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9f89165d00686f13f7d15d2cc4dffa",
+    "url": "/create-react-app/index.html"
+  },
+  {
+    "revision": "d9a94434153d9dbd5164",
+    "url": "/create-react-app/static/css/main.df80b2f5.chunk.css"
+  },
+  {
+    "revision": "896d17f2ca83ec3f3763",
+    "url": "/create-react-app/static/js/2.3138836b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/create-react-app/static/js/2.3138836b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a94434153d9dbd5164",
+    "url": "/create-react-app/static/js/main.3534c2ec.chunk.js"
+  },
+  {
+    "revision": "c5b0cf7d3ab2ef4a283c",
+    "url": "/create-react-app/static/js/runtime-main.98e9b92b.js"
+  },
+  {
+    "revision": "5e065a45aff933ecba1164ebaec578c6",
+    "url": "/create-react-app/static/media/bg.5e065a45.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/create-react-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/create-react-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/create-react-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/create-react-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/create-react-app/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
